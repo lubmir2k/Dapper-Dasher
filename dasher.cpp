56f86bb34092245774a887fe25490730d40794ed
lubmir2k <lubomir.mitana@protonmail.com>
2021-12-25 12:39:16 +0100

    Movement + gravity

--- dasher.cpp
+++ dasher.cpp
@@ -6,6 +6,15 @@ int main()
     const int winHeight{380};
 
     InitWindow(winWidth, winHeight, "Dapper Dasher");
+
+    // Acceleration due to gravity (pixels/frame) per frame
+    const int gravity{1};
+
+    const int width{50};
+    const int height{80};
+
+    int pos_y {winHeight - height};
+    int velocity{0};
     
     SetTargetFPS(30);
 
@@ -13,6 +22,18 @@ int main()
     {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        if(IsKeyPressed(KEY_SPACE))
+        {
+            velocity -= 10;
+        }
+
+        // Apply gravity
+        velocity += gravity;
+        
+        // Update position
+        pos_y += velocity;
+        DrawRectangle(winWidth/2, pos_y, width, height, BLUE);
         EndDrawing();
     }
     CloseWindow();
