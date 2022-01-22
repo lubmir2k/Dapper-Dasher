4e61515ad9f9e79b19a8f757e903273070404465
lubmir2k <lubomir.mitana@protonmail.com>
2022-01-22 20:00:52 +0100

    Added nebula

--- dasher.cpp
+++ dasher.cpp
@@ -4,12 +4,23 @@ int main()
 {
     constexpr int window_width{512};
     constexpr int window_height{380};
+
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
     
     InitWindow(window_width, window_height, "Dapper Dasher");
 
     // Acceleration due to gravity (pixels per second)
-    const int gravity{1'000};
+    const int gravity {1'000};
 
+    // Nebula variables
+    Texture2D nebulaTex = LoadTexture("assets/12_nebula_spritesheet.png");
+    Rectangle nebRec{0, 0, nebulaTex.width / 8, nebulaTex.height / 8};
+    Vector2 nebPos { window_width, window_height - nebRec.height};
+
+    // Nebula x velocity (pixels/second)
+    int nebVel { -600 };
+
+    // Scarfy variables
     Texture2D scarfyTex = LoadTexture("assets/testA_Twirl hair_0deg.png");
     Rectangle scarfyRec;
     Vector2 frameSize {138, 197 };
@@ -26,6 +37,7 @@ int main()
     scarfyPos.y = window_height - scarfyRec.height;
 
     int frame{};
+    
     // Amount of time before updating animation frame - one twelfth of a second
     const float updateTime{static_cast<float>(1.0 / 60.0)};
     float runningTime{};
@@ -63,30 +75,39 @@ int main()
             velocity += jumpVel;
         }
 
-
-        // Update the position
+        // Update nebula's position
+        nebPos.x += nebVel * deltaTime;
+        
+        // Update Scarfy's position
         scarfyPos.y += velocity * deltaTime;
 
-        // Update running time
-        runningTime += deltaTime;
-        if (runningTime >= updateTime)
-        {
-            runningTime = 0.0;
+       if (!isInAir)
+       {
+           // Update running time
+           runningTime += deltaTime;
+           if (runningTime >= updateTime)
+           {
+               runningTime = 0.0;
 
-            // Update the animation frame
-            //scarfyRec.x = frame * scarfyRec.width;
-            scarfyRec.x =(frame % framesWide) * frameSize.x;
-            scarfyRec.y = (int)(frame / framesWide) * frameSize.y;
-            frame++;
-            if (frame > maxFrame)
-            {
-                frame = 0;
-            }
-        }
+               // Update the animation frame
+               //scarfyRec.x = frame * scarfyRec.width;
+               scarfyRec.x =(frame % framesWide) * frameSize.x;
+               scarfyRec.y = (int)(frame / framesWide) * frameSize.y;
+               frame++;
+               if (frame > maxFrame)
+               {
+                   frame = 0;
+               }
+           }
+       }
+        // Draw nebula
+        DrawTextureRec(nebulaTex, nebRec, nebPos, WHITE);
 
+        // Draw Scarfy
         DrawTextureRec(scarfyTex, scarfyRec, scarfyPos, WHITE);
         EndDrawing();
     }
     UnloadTexture(scarfyTex);
+    UnloadTexture(nebulaTex);
     CloseWindow();
 }
